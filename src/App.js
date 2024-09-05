d20123c51f8253a7063db2ddcee093dd25bdedb6
Muhammd Maaz <161757387+maaz-official@users.noreply.github.com>
2024-09-05 10:17:35 -0700

    PIN CODE COMPONENT IS UPDATED V2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StylishInput from './components/pinCode'; // Import the component
+
+const App = () => {
+  return (
+    <div>
+      <h1>GOOWRITE.COM COMPONENTS</h1>
+      <StylishInput />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pinCode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pinCode.css';
+
+const StylishInput = () => {
+  return (
+    <div className="input-container">
+      <input type="text" maxLength="6" />
+    </div>
+  );
+};
+
+export default StylishInput;
